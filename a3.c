eb3ceebd0ed2bd9c12af8108ade70543a9acdf94
Alex <andres.al.campos@gmail.com>
2018-06-26 11:46:06 -0700

    finished the cowndown timer and the wall clock

--- a3.c
+++ a3.c
@@ -4,21 +4,43 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+
+time_t rawtime;
+int hours, minutes, seconds;
 
 void* monitorCountdownTimer(void* runTime){
-	printf("tmonitorCountdownTimer\n");
+	int iterations = *(int*)runTime;
+	hours = iterations / 3600;
+	minutes = (iterations / 60) % 60;
+	seconds = iterations % 60;
+	int i;
+	for(i = 0; i <= iterations ; i++){
+		printf("Countdown: %d:%d:%d\n", hours, minutes, seconds);
+		if (--seconds == -1){ //decrease the seconds and set them back to 59 if a minute is over
+			seconds = 59;
+			if (--minutes == -1){ //decrease the minutes and set them back to 59 if an hour is over
+				minutes = 59;
+				hours--; //decrease the ours
+			}
+		}
+		sleep(1);
+	}
+	printf("You're late! You're late! For a very important date!\n");
+	//kill sibling
+	//commit sudoku
 }
 
 void* wallClock(void* runTime){
 	int iterations = *(int*)runTime;
-	int i = 0;
-	for(i = 0; i < iterations ; i++){
-		time_t rawtime;
-		struct tm *time = localtime(&rawtime);
-		printf("the current itme is: %d:%d:%d\n", time->tm_hour, time->tm_min, time->tm_sec);
+	int i;
+	//print the time every second the for as long as the user specified
+	for(i = 0; i <= iterations; i++){
+		time(&rawtime); //get the current time 
+		struct tm *time = localtime(&rawtime); //store it in a struct to get only hrs,min,sec
+		printf("The current time is: %d:%d:%d \n", time->tm_hour, time->tm_min, time->tm_sec);
 		sleep(1);
 	}
-	printf("Alarm activated\n");
 }
 
 int main(int argc, char * argv[]){
@@ -29,10 +51,7 @@ int main(int argc, char * argv[]){
 	} else {
 		runTime = 25;
 	}
-
-	printf("satrting wallClock\n");
 	pthread_create(&thread1, NULL, monitorCountdownTimer, &runTime); 
-	printf("starting tmonitorCountdownTimer\n");
 	pthread_create(&thread2, NULL, wallClock, &runTime); 	
 
 	pthread_join(thread1, NULL);
@@ -40,4 +59,3 @@ int main(int argc, char * argv[]){
 	
 	printf("A friendly message.\n");
 }
-
